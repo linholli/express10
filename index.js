ed55af84434406218c717db2f7532727a5428c45
Lin <aaaa91103@gmail.com>
2024-03-12 12:20:03 +0800

    init2

---
+++ index.js
@@ -0,0 +1,23 @@
+// import express from "express";
+// import {resolve} from "path";
+// const __dirname = import.meta.dirname;
+
+const express = require ("express");
+const path = require ("path");
+// const __dirname = import.meta.dirname;
+
+
+const app = express();
+app.use(express.static(path.resolve(__dirname, "public")));
+app.use("/bootstrap", express.static(path.resolve(__dirname, "node_modules/bootstrap/dist")));
+app.use("/jquery", express.static(path.resolve(__dirname, "node_modules/jquery/dist")))
+app.use('/fontawesome', express.static(path.resolve(__dirname, 'node_modules/@fortawesome/fontawesome-free')));
+
+app.get("/", (req, res) => {
+  res.send("網站首頁")
+});
+
+
+app.listen(3000, () => {
+  ;console.log("伺服器已啟動於 http://localhost:3000");
+})
